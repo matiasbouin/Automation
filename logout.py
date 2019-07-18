99d5d8e18fc92b672bf09e3a2306234792a69d16
Matias Bouin <49533289+matiasbouin@users.noreply.github.com>
2019-07-18 00:00:33 -0300

    GUI automated logout

---
+++ logout.py
@@ -0,0 +1,12 @@
+#! python3
+
+'''
+A program that logs out from the current windows 10 user
+'''
+
+import pyautogui
+
+pyautogui.click(25, 1005, duration = 1)
+pyautogui.click(25, 768, duration = 1)
+pyautogui.doubleClick(25, 595, duration = 1)
+
